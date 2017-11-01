4f218d0753a01f59eda2bd6e6c60abe5f9da87da
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2017-11-02 00:29:37 +0700

    Update index.js

--- src/calendar/header/index.js
+++ src/calendar/header/index.js
@@ -85,7 +85,8 @@ class CalendarHeader extends Component {
           {leftArrow}
           <View style={{ flexDirection: 'row' }}>
             <Text style={this.style.monthText}>
-              {this.props.month.toString(this.props.monthFormat ? this.props.monthFormat : 'MMMM yyyy')}
+              {this.props.month.toString(this.props.monthFormat ? this.props.monthFormat : 'MMMM ')}
+              {parseInt(this.props.month.toString(this.props.monthFormat ? this.props.monthFormat : 'yyyy')) + 543}
             </Text>
             {indicator}
           </View>
